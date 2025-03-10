f85ed1d6579302fc95667bd445282a94fa94d0b2
luisgrange <luisfernando237grange@outlook.com>
2025-03-10 13:26:23 -0300

    Adicionar arquivos de projeto.

---
+++ MicroQueueAPI/Controllers/QueueController.cs
@@ -0,0 +1,31 @@
+﻿using MicroQueueAPI.Event;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroQueueAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QueueController : ControllerBase
+    {
+
+        private readonly RabbitMQEvent _event;
+
+        public QueueController(RabbitMQEvent rabbitMqEvent)
+        {
+            _event = rabbitMqEvent;
+        }
+
+        [HttpPost("send")]
+        public IActionResult SendMessage([FromBody] string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                return BadRequest("Message cannot be null or empty.");
+            }
+
+            _event.PublishMessage("microqueue", message);
+            return Ok("message sent to RabbitMQ");
+        }
+
+    }
+}
---
+++ MicroQueueAPI/Events/RabbitMQEvent.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace MicroQueueAPI.Event
+{
+    public class RabbitMQEvent
+    {
+        private readonly string _hostName;
+        private readonly string _userName;
+        private readonly string _password;
+
+        public RabbitMQEvent(IConfiguration configuration)
+        {
+            _hostName = configuration["RabbitMQ:HostName"];
+            _userName = configuration["RabbitMQ:UserName"];
+            _password = configuration["RabbitMQ:Password"];
+        }
+
+        public void PublishMessage(string queueName, string message)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _hostName,
+                UserName = _userName,
+                Password = _password
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: queueName,
+                                     basicProperties: null,
+                                     body: body);
+            }
+        }
+    }
+}
